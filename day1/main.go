493f05e76f288d5570346c5e8a77104cd24375d7
Darrian <3525311+rikkuness@users.noreply.github.com>
2021-12-01 22:32:59 +0000

    day1

---
+++ day1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, _ := os.Open("input.txt")
+	s := bufio.NewScanner(f)
+	var c, l, t int = 0, int(math.Inf(1)), 0
+	for s.Scan() {
+		c, _ = strconv.Atoi(s.Text())
+		if c > l {
+			t++
+		}
+		l = c
+	}
+	fmt.Println(t)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rikkuness/aoc-2021
+
+go 1.16
