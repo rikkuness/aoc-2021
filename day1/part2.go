c1fdc0ea02da1f38bca4037995711ac23671156f
Darrian <3525311+rikkuness@users.noreply.github.com>
2021-12-01 23:38:10 +0000

    add part 2

---
+++ day1/part2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func part2() {
+	f, _ := os.Open("input.txt")
+	s := bufio.NewScanner(f)
+	var (
+		w       [3]int
+		t, i, l int = 0, 0, int(math.Inf(1))
+	)
+	for s.Scan() {
+		c, _ := strconv.Atoi(s.Text())
+		w[2] = w[1]
+		w[1] = w[0]
+		w[0] = c
+		i++
+		if i < 3 {
+			continue
+		}
+		a := w[0] + w[1] + w[2]
+		if a > l {
+			t++
+		}
+		l = a
+	}
+	fmt.Println(t)
+}
