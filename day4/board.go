02582cc7aa506484d4cc5fd33f648fdb3ca28a3e
Darrian <3525311+rikkuness@users.noreply.github.com>
2021-12-04 13:38:03 +0000

    that's a bingo

---
+++ day4/board.go
@@ -0,0 +1,67 @@
+package main
+
+type Cell struct {
+	val  uint8
+	tick bool
+}
+
+type Board struct {
+	data [d][d]Cell
+	done bool
+}
+
+func (b *Board) Mark(i uint8) {
+	for x := 0; x < d; x++ {
+		for y := 0; y < d; y++ {
+			if b.data[x][y].val == i {
+				b.data[x][y].tick = true
+				return
+			}
+		}
+	}
+}
+
+func (b *Board) rowDone(rn int) bool {
+	t := 0
+	for i := 0; i < d; i++ {
+		if b.data[rn][i].tick {
+			t++
+		}
+	}
+	return t == d
+}
+
+func (b *Board) colDone(cn int) bool {
+	t := 0
+	for i := 0; i < d; i++ {
+		if b.data[i][cn].tick {
+			t++
+		}
+	}
+	return t == d
+}
+
+func (b *Board) Bingo() bool {
+	for i := 0; i < d; i++ {
+		if b.rowDone(i) {
+			b.done = true
+			return true
+		}
+		if b.colDone(i) {
+			b.done = true
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) Sum() (total int) {
+	for x := 0; x < d; x++ {
+		for y := 0; y < d; y++ {
+			if !b.data[x][y].tick {
+				total += int(b.data[x][y].val)
+			}
+		}
+	}
+	return
+}
---
+++ day4/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const d = 5
+
+func main() {
+	f, _ := os.Open("input.txt")
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanWords)
+
+	var callouts []uint8
+	boards := make([]*Board, 0)
+
+	for i := -1; s.Scan(); i++ {
+		if i < 0 {
+			for _, v := range strings.Split(s.Text(), ",") {
+				ui, _ := strconv.ParseUint(v, 10, 8)
+				callouts = append(callouts, uint8(ui))
+			}
+			continue
+		}
+
+		ui, _ := strconv.ParseUint(s.Text(), 10, 8)
+		if len(boards) <= i/(d*d) {
+			boards = append(boards, &Board{})
+		}
+
+		boards[i/(d*d)].data[(i%(d*d)/d)%d][i%(d*d)%d].val = uint8(ui)
+	}
+
+	for _, c := range callouts {
+		for bi, b := range boards {
+			if !b.done {
+				b.Mark(c)
+				if b.Bingo() {
+					fmt.Println("board", bi, "wins: ", b.Sum()*int(c))
+				}
+			}
+		}
+	}
+}
